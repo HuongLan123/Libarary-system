44d057e999e56181d6529d8ef6f3d72d9d55d467
Phạm Lan Hương <hustdream2k5@gmail.com>
2025-06-08 01:24:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-# Import các hàm giao diện menu và các chức năng quản lý từ các module tương ứng
-from menu import menu, book_management, reader_management, loan_management
-
 # Import các hàm kết nối và nạp lại dữ liệu từ cơ sở dữ liệu
 from database import create_connection, reload_database_book, reload_database_loan, reload_database_reader
-
-# Import lớp và trình quản lý mượn sách
-from loan import LoanRecord, LoanManager
-
-def main():
-    # Tạo kết nối đến cơ sở dữ liệu SQLite
+def connect():
+      # Tạo kết nối đến cơ sở dữ liệu SQLite
     conn, cursor = create_connection("library11.db")
-
     # Kiểm tra kết nối thành công hay không
     if not conn:
         print("Không thể kết nối đến cơ sở dữ liệu.")
+        return False, None, None
     else:
+        from database import create_table_database
+        create_table_database(conn, cursor)
+        return True, conn, cursor
+def main():
+    connected, conn, cursor = connect()
+    if connected: 
+        # Import các hàm giao diện menu và các chức năng quản lý từ các module tương ứng
+        from menu import menu, book_management, reader_management, loan_management
+
         # Vòng lặp chính của chương trình để xử lý các lựa chọn chức năng
         while True: 
             menu()  # Hiển thị menu chức năng
@@ -22,17 +24,19 @@ def main():
 
             if ch == "1":
                 print("Quản lý sách")
-                reload_database_book()       # Nạp dữ liệu sách từ cơ sở dữ liệu vào bộ nhớ
+                reload_database_book(conn, cursor)       # Nạp dữ liệu sách từ cơ sở dữ liệu vào bộ nhớ
                 book_management()            # Gọi giao diện quản lý sách
 
             elif ch == "2":
                 print("Quản lý bạn đọc")
-                reload_database_reader()     # Nạp dữ liệu bạn đọc từ cơ sở dữ liệu vào bộ nhớ
+                reload_database_reader(conn, cursor)     # Nạp dữ liệu bạn đọc từ cơ sở dữ liệu vào bộ nhớ
                 reader_management()          # Gọi giao diện quản lý bạn đọc
 
             elif ch == "3":
+                # Import lớp và trình quản lý mượn sách
+                from loan import LoanRecord, LoanManager
                 print("Quản lý mượn trả sách")
-                reload_database_loan(LoanManager)  # Nạp dữ liệu mượn trả vào bộ nhớ với LoanManager
+                reload_database_loan(LoanManager, conn, cursor)  # Nạp dữ liệu mượn trả vào bộ nhớ với LoanManager
                 loan_management()                 # Gọi giao diện quản lý mượn trả sách
 
             elif ch == "4":
