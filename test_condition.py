1eaaae3c00486a674c8e3d9d5fe707726471b7c2
Phạm Lan Hương <hustdream2k5@gmail.com>
2025-06-07 23:39:27 +0700

    Update test_condition.py

--- test_condition.py
+++ test_condition.py
@@ -1,3 +1,9 @@
+# import thư viện, cấu trúc dữ liệu 
+from cautrucdulieu import BSTree
+from loan import LoanRecord
+from datetime import datetime
+
+# Hàm kiểm tra tính logic trong dữ liệu sách
 def test_book(isbn, title, genre, author, added_quantity, quantity, available_quantity, borrowed_quantity):
     """
     Kiểm tra dữ liệu hợp lệ của một quyển sách trước khi thêm vào hệ thống.
@@ -37,3 +43,35 @@ def test_book(isbn, title, genre, author, added_quantity, quantity, available_qu
         return False, "Tổng số lượng phải bằng đã mượn + có sẵn."
 
     return True, ""
+
+# Hàm kiểm tra tính logic trong dữ liệu người đọc
+def test_reader(reader_table, reader_id, name):
+    # Kiểm tra về trống dữ liệu trong reader_id và name
+    if not reader_id or not name:
+        print("❌ Mã số sinh viên và tên không được để trống.")
+        return
+    # Kiểm tra sự tồn tại của bạn đọc trong bảng
+    if reader_table.search(reader_id):
+        print("❌ Bạn đọc đã tồn tại.")
+        return
+    
+# Hàm kiểm tra điều kiện để tạo phiếu mượn
+def can_borrow(self, reader_id, isbn):
+    # Kiểm tra sự tồn tại của bạn đọc
+    if reader_id not in self.reader_cache:
+        print(f"❌ Bạn đọc '{reader_id}' không tồn tại.")
+        return False
+    # Kiểm tra sự tồn tại của sách
+    if isbn not in self.book_cache:
+        print(f"❌ Sách với ISBN '{isbn}' không tồn tại.")
+        return False
+    # Kiểm tra số lượng còn của sách
+    if self.book_cache[isbn] <= 0:
+        print("❌ Sách đã hết, không thể mượn.")
+        return False
+    # Kiểm tra về tình trạng mượn sách của bạn đọc
+    for loan in self.tree.inorder():
+        if loan.reader_id == reader_id and loan.isbn == isbn and loan.status == "Đang mượn":
+            print("❌ Bạn đọc đang mượn sách này và chưa trả.")
+            return False
+    return True
